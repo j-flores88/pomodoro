63cf05ccd32d3e93f951a126c47fd41d63638063
Joel Flores <j_flores88@outlook.com>
2020-09-17 19:35:16 -0700

    Each individual tomato disapears as you click on it.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let pomoArr = []
+let pomosNeeded
 
 const pomoArea = document.getElementById('pomodoros');
-const toms = document.getElementById('tomato');
+const toms = document.getElementById('img');
 const userInput = document.querySelector('input');
 const enterBtn = document.getElementById('enterBtn');
 const reseBtn = document.getElementById('reset');
@@ -10,7 +11,7 @@ $(reseBtn).hide();
 
 const fillArr = function(amount){
     amount = document.getElementById('userInput').value
-    let pomosNeeded = amount * 2;
+    pomosNeeded = amount * 2;
     
     for(let i = 0; i < pomosNeeded; i++) {
         pomoArr.push(toms)
@@ -18,12 +19,20 @@ const fillArr = function(amount){
     fillPomos()
 }
 const fillPomos = function() {
+
     for(let j = 0; j < pomoArr.length; j++) {
         let img = document.createElement('img')
+
+
         img.style.width = "200px";
         img.style.width = "200px";
         img.setAttribute('src', "./resources/tomato.png");
+
         pomoArea.appendChild(img)
+
+        img.onclick = function() {
+            $(this).fadeOut()
+        }
     }
 }
 
@@ -32,12 +41,13 @@ enterBtn.onclick = function() {
     $(enterBtn).fadeOut();
     $(reseBtn).fadeIn();
     $(userInput).fadeOut();
-    $(userInput).val('');
+    $(userInput).val('');  
 }
 
 reseBtn.onclick = function() {
     $(enterBtn).fadeIn();
     $(userInput).fadeIn();
     pomoArea.innerHTML = ""
+    pomoArr = []
     $(reseBtn).hide();
 }
