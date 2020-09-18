d52b0f4425da682fafafa5ca381ef1fa4e042ade
Joel Flores <j_flores88@outlook.com>
2020-09-17 20:54:18 -0700

    Added timer for studying and breaks.

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const toms = document.getElementById('img');
 const userInput = document.querySelector('input');
 const enterBtn = document.getElementById('enterBtn');
 const reseBtn = document.getElementById('reset');
+const timeDis = document.getElementById('display');
+const brkBtn = document.getElementById('breakBtn');
+const pomoBtn = document.getElementById('pomoBtn');
+const stopBtn = document.getElementById('stpBtn');
 
 $(reseBtn).hide();
 
@@ -35,6 +39,44 @@ const fillPomos = function() {
         }
     }
 }
+
+const startTimer = function(duration, display) {
+    let timer = duration, minutes, seconds;
+
+    setInterval(function(){
+        minutes =  parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        timeDis.textContent = minutes + ':' + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000)
+
+    stopBtn.onclick = function() {
+        console.log('stop')
+        clearInterval()
+    }
+}
+
+brkBtn.onclick = function() {
+    let fiveMins = 60 * 5;
+    let display = timeDis;
+
+    startTimer(fiveMins, display)
+}
+
+pomoBtn.onclick = function() {
+    let pomoMins = 60 * 25;
+    let display = timeDis;
+
+    startTimer(pomoMins, display)
+}
+
 
 enterBtn.onclick = function() {
     fillArr();
